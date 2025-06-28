67a7911969eb43873db3623dd8cc9ab55ce67b2a
EfrenDaMo <david.mora.work@gmail.com>
2025-06-27 18:13:28 -0600

    Otro cambio minimo

--- src/memorama/ui/VistaMenuPrincipal.java
+++ src/memorama/ui/VistaMenuPrincipal.java
@@ -7,7 +7,6 @@ package memorama.ui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
